682e50053c8f476b6d4951c316172d9bb131fef4
Luca <luca.malattia@gmail.com>
2021-01-15 09:06:25 +0100

    update secure resource in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,13 @@
 
         <title>Image Uploader</title>
 
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/app.css', true) }}" rel="stylesheet">
     </head>
     <body class="antialiased">
         <div id="app" class="bg-gray-200 min-h-screen flex justify-center items-center">
             <image-uploader />
         </div>
 
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ asset('js/app.js', true) }}"></script>
     </body>
 </html>
